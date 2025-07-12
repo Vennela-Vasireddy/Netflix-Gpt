f090b1b5c90cde685ca95c184be5820cedd7acc2
Vennela-HospiceMD <vvasireddy@hospicemd.com>
2025-07-12 13:49:36 -0700

    Updated gptSlice with a reducer

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -5,15 +5,24 @@ const gptSlice = createSlice({
 name: 'gpt',
   initialState: {
     showGptSearch: false,
+    movieNames: null,
+     movieResults: null
 
   },
   reducers: {
     toggleGptSearchView: (state, action) => {
       state.showGptSearch = !state.showGptSearch;
+    },
+
+    addGptMovieResult: () => {
+      const[movieNames, movieResults] = action.payload;
+      movieNames = action.movieNames
+      movieResults = action.movieResults
+
     }
 
 } 
 })
 
-export const { toggleGptSearchView } = gptSlice.actions;
+export const { toggleGptSearchView, addGptMovieResult } = gptSlice.actions;
 export default gptSlice.reducer;
