356f0437fe45a950de73c9effa67dc022423c54b
Vennela Vasireddy <64094167+Vennela-Vasireddy@users.noreply.github.com>
2025-10-13 15:21:46 -0700

    Update MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import VideoTitle from './videoTitle';
-import VideoBackground from './videoBackground';
+import VideoTitle from "./videoTitle";
+import VideoBackground from "./videoBackground";
 
 const MainContainer = () => {
 
